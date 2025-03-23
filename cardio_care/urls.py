1f3d3036c9e2c9005ddcb07c66236912f9b48933
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-03-23 20:17:26 +0700

    update

--- cardio_care/urls.py
+++ cardio_care/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('login', views.login),
-    re_path('register', views.register),
-    re_path('test-token', views.test_token),
     path('', include('main.urls')),
 ]
