ac0b6dc225de4e4ee37c15c4eeb88e204f102840
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-03-28 14:22:04 +0700

    ubah type field id obat ke uuid

--- beli_obat/models.py
+++ beli_obat/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Obat(models.Model):
+    id = models.UUIDField(primary_key=True, default = uuid.uuid4, editable = False)
     nama_obat = models.CharField(max_length=255)
     harga = models.IntegerField()
     deskripsi = models.TextField(max_length=1000)
