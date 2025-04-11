7af0364bdac4e18064a7b207056982308d85d7bf
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-04-11 17:40:53 +0700

    update settings pakai env var

--- cardio_care/settings.py
+++ cardio_care/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cardiocare2025@gmail.com'  # Email pengirim
-EMAIL_HOST_PASSWORD = 'pibzpdhseabvcbmb'  
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
