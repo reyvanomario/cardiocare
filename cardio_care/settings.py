2d2f4ef9b97f7504bf4ae7d1901b5f75bc647f0c
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-04-11 20:55:22 +0700

    update settings

--- cardio_care/settings.py
+++ cardio_care/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
-#ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kelompok-38-cardiocare.pkpl.cs.ui.ac.id"]
 
 
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "https://127.0.0.1").split(",")
+# CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "https://127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = ["https://kelompok-38-cardiocare.pkpl.cs.ui.ac.id"]
 
 # Application definition
 
