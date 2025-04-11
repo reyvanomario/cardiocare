5641e31ccf682d0d7e7e64ef6000c9b72b496aa6
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-04-11 21:41:39 +0700

    test hardcode variable settings

--- cardio_care/settings.py
+++ cardio_care/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-bk=40re(22ir)7btki29_-^7hr6fq(s!)$ct2#e52fz#_%81n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
