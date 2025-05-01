1b15e455a76c4657cea4993ba323d5526363c6fa
Reyvano Mario Sianturi <reyvano.mario@ui.ac.id>
2025-05-01 12:13:45 +0700

    edit debug true sementara

--- cardio_care/settings.py
+++ cardio_care/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+# DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
 
 # USE_X_FORWARDED_HOST = True  # Penting untuk mengenali host dari Ingress
